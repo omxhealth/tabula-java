71cbdce12f0a1d115c3a623ecb582ea04b525afa
Alex Wilson <aewilson@ualberta.ca>
2016-09-15 16:50:28 -0400

    Add column regions to debugger.
     * split column region & edge calculations in BasicExctractionAlgorithm
     * add -C option to Debug to allow displaying column regions

--- src/main/java/technology/tabula/extractors/BasicExtractionAlgorithm.java
+++ src/main/java/technology/tabula/extractors/BasicExtractionAlgorithm.java
@@ -15,16 +15,16 @@ import technology.tabula.TextChunk;
 import technology.tabula.TextElement;
 
 public class BasicExtractionAlgorithm implements ExtractionAlgorithm {
-    
+
     private List<Ruling> verticalRulings = null;
-    
+
     public BasicExtractionAlgorithm() {
     }
-    
+
     public BasicExtractionAlgorithm(List<Ruling> verticalRulings) {
         this.verticalRulings = verticalRulings;
     }
-    
+
     public List<Table> extract(Page page, List<Float> verticalRulingPositions) {
         List<Ruling> verticalRulings = new ArrayList<Ruling>(verticalRulingPositions.size());
         for (Float p: verticalRulingPositions) {
@@ -36,17 +36,17 @@ public class BasicExtractionAlgorithm implements ExtractionAlgorithm {
 
     @Override
     public List<Table> extract(Page page) {
-        
+
         List<TextElement> textElements = page.getText();
-        
+
         if (textElements.size() == 0) {
             return Arrays.asList(new Table[] { Table.EMPTY });
         }
-        
+
         List<TextChunk> textChunks = this.verticalRulings == null ? TextElement.mergeWords(page.getText()) : TextElement.mergeWords(page.getText(), this.verticalRulings);
         List<Line> lines = TextChunk.groupByLines(textChunks);
         List<Float> columns = null;
-        
+
         if (this.verticalRulings != null) {
             Collections.sort(this.verticalRulings, new Comparator<Ruling>() {
                 @Override
@@ -62,13 +62,13 @@ public class BasicExtractionAlgorithm implements ExtractionAlgorithm {
         else {
             columns = columnPositions(lines);
         }
-        
+
         Table table = new Table(page, this);
-        
+
         for (int i = 0; i < lines.size(); i++) {
             Line line = lines.get(i);
             List<TextChunk> elements = line.getTextElements();
-            
+
             Collections.sort(elements, new Comparator<TextChunk>() {
 
 				@Override
@@ -76,7 +76,7 @@ public class BasicExtractionAlgorithm implements ExtractionAlgorithm {
 					return new java.lang.Float(o1.getLeft()).compareTo(o2.getLeft());
 				}
 			});
-            
+
             for (TextChunk tc: elements) {
                 if (tc.isSameChar(Line.WHITE_SPACE_CHARS)) {
                     continue;
@@ -86,47 +86,42 @@ public class BasicExtractionAlgorithm implements ExtractionAlgorithm {
                 boolean found = false;
                 for(; j < columns.size(); j++) {
                     if (tc.getLeft() <= columns.get(j)) {
-                        found = true; 
+                        found = true;
                         break;
-                    } 
+                    }
                 }
                 table.add(tc, i, found ? j : columns.size());
             }
         }
-        
+
         return Arrays.asList(new Table[] { table } );
     }
-    
+
     @Override
     public String toString() {
         return "basic";
     }
-    
-    
-    /**
-     * @param lines must be an array of lines sorted by their +top+ attribute
-     * @return a list of column boundaries (x axis)
-     */
-    public static List<java.lang.Float> columnPositions(List<Line> lines) {
 
+
+    public static List<Rectangle> columnRegions(List<Line> lines) {
         List<Rectangle> regions = new ArrayList<Rectangle>();
         for (TextChunk tc: lines.get(0).getTextElements()) {
-            if (tc.isSameChar(Line.WHITE_SPACE_CHARS)) { 
-                continue; 
+            if (tc.isSameChar(Line.WHITE_SPACE_CHARS)) {
+                continue;
             }
             Rectangle r = new Rectangle();
             r.setRect(tc);
             regions.add(r);
         }
-        
+
         for (Line l: lines.subList(1, lines.size())) {
             List<TextChunk> lineTextElements = new ArrayList<TextChunk>();
             for (TextChunk tc: l.getTextElements()) {
-                if (!tc.isSameChar(Line.WHITE_SPACE_CHARS)) { 
+                if (!tc.isSameChar(Line.WHITE_SPACE_CHARS)) {
                     lineTextElements.add(tc);
                 }
             }
-            
+
             for (Rectangle cr: regions) {
 
                 List<TextChunk> overlaps = new ArrayList<TextChunk>();
@@ -135,30 +130,38 @@ public class BasicExtractionAlgorithm implements ExtractionAlgorithm {
                         overlaps.add(te);
                     }
                 }
-                
+
                 for (TextChunk te: overlaps) {
                     cr.merge(te);
                 }
-                
+
                 lineTextElements.removeAll(overlaps);
             }
-            
+
             for (TextChunk te: lineTextElements) {
                 Rectangle r = new Rectangle();
                 r.setRect(te);
                 regions.add(r);
             }
         }
-        
+        return regions;
+      }
+
+    /**
+     * @param lines must be an array of lines sorted by their +top+ attribute
+     * @return a list of column boundaries (x axis)
+     */
+    public static List<java.lang.Float> columnPositions(List<Line> lines) {
+        List<Rectangle> regions = columnRegions(lines);
         List<java.lang.Float> rv = new ArrayList<java.lang.Float>();
         for (Rectangle r: regions) {
             rv.add((float) r.getRight());
         }
-        
+
         Collections.sort(rv);
-        
+
         return rv;
-        
+
     }
 
 }
